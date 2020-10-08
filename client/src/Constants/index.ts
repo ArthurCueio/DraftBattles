f95845dabb2afbf261c5e2fc28a9c00000187f1c
Arthur Coelho <arthurcoelho@pm.me>
2020-10-08 16:16:14 -0300

    Prettier formating

--- client/src/Constants/index.ts
+++ client/src/Constants/index.ts
@@ -1,3 +1,4 @@
-const DDGRADON_BASE_URL = "http://ddragon.leagueoflegends.com/cdn/10.20.1/img/champion/";
+const DDGRADON_BASE_URL =
+  "http://ddragon.leagueoflegends.com/cdn/10.20.1/img/champion/";
 
-export {DDGRADON_BASE_URL}
+export { DDGRADON_BASE_URL };
